82a613a5775fd0e1902063a43b175d8dff8d66ce
Vlad Vovk <vladyslav.vovk@bluechipsys.com>
2017-10-29 17:07:40 +0200

    Added news pagination

--- SimpleRedditClient/NewsFeed/Controllers/SRCNewsTableViewController.swift
+++ SimpleRedditClient/NewsFeed/Controllers/SRCNewsTableViewController.swift
@@ -31,9 +31,16 @@ class SRCNewsTableViewController: UITableViewController {
     func refreshNews() {
         printEnterLog()
     
+        guard !newsDownloadingInProgress else {
+            print("\(getFileAndFunctionTitle()). Can't start refreshing news because it's already in progress.")
+            return
+        }
+    
+        newsDownloadingInProgress = true
         refreshControl?.beginRefreshing()
         newsService?.getTopNews(completion: { [weak self] (posts, error) in
             self?.refreshControl?.endRefreshing()
+            self?.newsDownloadingInProgress = false
             guard error == nil else {
                 // TODO: Show alert
                 return
@@ -58,15 +65,56 @@ class SRCNewsTableViewController: UITableViewController {
     }
 
     // MARK: UITableViewDelegate
+    override func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+        let numOfPosts = self.posts?.count ?? 0
+        if indexPath.row + 1 == numOfPosts {
+            let spinner = UIActivityIndicatorView(activityIndicatorStyle: .gray)
+            spinner.startAnimating()
+            spinner.frame = CGRect(x: CGFloat(0), y: CGFloat(0), width: tableView.bounds.width, height: CGFloat(44))
+            tableView.tableFooterView = spinner
+        
+            getMoreNews(completion: { (posts, error) in
+                DispatchQueue.main.async {
+                    tableView.tableFooterView = UIView()
+                }
+            })
+        }
+    }
     
     /////////////////////////////////////////
     // MARK: PRIVATE
     // MARK: Accessors
+    var newsDownloadingInProgress = false
     var posts: [SRCPost]? {
         didSet {
             DispatchQueue.main.async { [weak self] in
                 self?.tableView.reloadData()
             }
         }
+    }
+    
+    // MARK: Helpers
+    private func getMoreNews(completion: @escaping ([SRCPost]?, Error?) -> Swift.Void) {
+        printEnterLog()
+        
+        guard !newsDownloadingInProgress else {
+            print("\(getFileAndFunctionTitle()). Can't start refreshing news because it's already in progress.")
+            return
+        }
+        
+        newsDownloadingInProgress = true
+        newsService?.getTopNewsNextBatch(completion: { [weak self] (posts, error) in
+            self?.newsDownloadingInProgress = false
+            guard error == nil else {
+                // TODO: Show alert
+                completion(posts, error)
+                return
+            }
+            
+            if let posts = posts {
+                self?.posts?.append(contentsOf: posts)
+            }
+            completion(posts, nil)
+        })
     }
 }
